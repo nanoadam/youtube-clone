d872c6194ea0c7ac061809f939d2363ce009c214
Nano Adam <nanoproductions2@gmail.com>
2020-09-18 09:38:50 -0400

    Working on video model

--- server/db/models/Video.js
+++ server/db/models/Video.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
-const VideoSchema = mongoose.Schema({});
+const VideoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Video title is required'],
+    minLength: [3, 'The title of the video must be at least 3 characters'],
+    maxLength: [225, 'The title of the video must be less than 225 characters'],
+  },
+  description: {
+    type: String,
+    maxLength: [5000, 'Description can max be 5000 characters'],
+    default: 'No description provided',
+  },
+  views: [
+    {
+      ref: {},
+    },
+  ],
+});
 
 module.exports = mongoose.model('Video', VideoSchema);
